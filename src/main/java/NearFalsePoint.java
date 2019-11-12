64bd4902319cac25bf3816843f4a5b5462e580c5
mryf <yazdanifar@ut.ac.ir>
2019-11-12 23:39:14 +0330

    Allow annotation repeating. gitignore added.

--- src/main/java/NearFalsePoint.java
+++ src/main/java/NearFalsePoint.java
@@ -1,10 +1,8 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(NearFalsePointContainer.class)
 public @interface NearFalsePoint {
 
     String predicate();
