5ac0ca7d779ef57d809477e928efde5b99d44194
mryf <yazdanifar@ut.ac.ir>
2019-11-13 10:51:29 +0330

    ClauseDefinition added.

---
+++ src/main/java/ClauseDefinition.java
@@ -0,0 +1,31 @@
+import java.lang.annotation.*;
+
+/**
+ * This annotation can be used for defining clauses. You can annotate either your SUT method or class or your test
+ * method or class with this annotation multiple times to define multiple clauses.
+ * <pre>
+ *     &#64;ClauseDefinition(clause = 'a', "age &lt; 23")
+ *     &#64;ClauseDefinition(clause = 'b', "door.state.equals(Door.State.CLOSED)")
+ *     &#64;Test
+ *     public void test1() {
+ *         ...
+ *     }
+ * </pre>
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(ClauseDefinitionContainer.class)
+public @interface ClauseDefinition {
+
+    /**
+     * Character which represents a clause
+     * @return clause to be defined.
+     */
+    char clause();
+
+    /**
+     * A string which describes a clause.
+     * @return string defining the clause
+     */
+    String def();
+}
